75838a4584f6daf2429751268ee65dce53e5236c
parwezshariq <shariqparwez@outlook.com>
2023-03-16 00:45:23 +0530

    Added JPA Repositories

---
+++ src/main/java/com/shariqparwez/spring6webapp/repositories/AuthorRepository.java
@@ -0,0 +1,7 @@
+package com.shariqparwez.spring6webapp.repositories;
+
+import com.shariqparwez.spring6webapp.domain.Author;
+import org.springframework.data.repository.CrudRepository;
+
+public interface AuthorRepository extends CrudRepository<Author, Long> {
+}
