16bc2b61baede44a54048c3a69535a9334d183e3
parwezshariq <shariqparwez@outlook.com>
2023-03-16 22:03:06 +0530

    Enabled H2 Console (in-memory-database)

--- src/main/java/com/shariqparwez/spring6webapp/bootstrap/BootstrapData.java
+++ src/main/java/com/shariqparwez/spring6webapp/bootstrap/BootstrapData.java
@@ -25,43 +25,45 @@ public class BootstrapData implements CommandLineRunner {
 
     @Override
     public void run(String... args) throws Exception {
-        Author eric = new Author();
-        eric.setFirstName("Eric");
-        eric.setLastName("Evans");
+        Author authorOne = new Author();
+        authorOne.setFirstName("Eric");
+        authorOne.setLastName("Evans");
 
-        Book ddd = new Book();
-        ddd.setTitle("Domain Driven Design");
-        ddd.setIsbn("123456");
+        Book bookOne = new Book();
+        bookOne.setTitle("Domain Driven Design");
+        bookOne.setIsbn("123456");
 
-        Author ericSaved = authorRepository.save(eric);
-        Book dddSaved = bookRepository.save(ddd);
+        Author authorOneSaved = authorRepository.save(authorOne);
+        Book bookOneSaved = bookRepository.save(bookOne);
 
-        Author rod = new Author();
-        rod.setFirstName("Rod");
-        rod.setLastName("Johnson");
+        Author authorTwo = new Author();
+        authorTwo.setFirstName("Rod");
+        authorTwo.setLastName("Johnson");
 
-        Book noEJB = new Book();
-        noEJB.setTitle("J2EE Development without EJB");
-        noEJB.setIsbn("54757585");
+        Book bookTwo = new Book();
+        bookTwo.setTitle("J2EE Development without EJB");
+        bookTwo.setIsbn("54757585");
 
-        Author rodSaved = authorRepository.save(rod);
-        Book noEJBSaved = bookRepository.save(noEJB);
+        Author authorTwoSaved = authorRepository.save(authorTwo);
+        Book bookTwoSaved = bookRepository.save(bookTwo);
 
-        ericSaved.getBooks().add(dddSaved);
-        rodSaved.getBooks().add(noEJBSaved);
+        authorOneSaved.getBooks().add(bookOneSaved);
+        authorTwoSaved.getBooks().add(bookTwoSaved);
+        bookOneSaved.getAuthors().add(authorOneSaved);
+        bookTwoSaved.getAuthors().add(authorTwoSaved);
 
         Publisher publisher = new Publisher();
         publisher.setPublisherName("My Publisher");
         publisher.setAddress("123 Main");
         Publisher savedPublisher = publisherRepository.save(publisher);
 
-        dddSaved.setPublisher(savedPublisher);
-        noEJBSaved.setPublisher(savedPublisher);
+        bookOneSaved.setPublisher(savedPublisher);
+        bookTwoSaved.setPublisher(savedPublisher);
 
-        authorRepository.save(ericSaved);
-        authorRepository.save(rodSaved);
-        bookRepository.save(dddSaved);
-        bookRepository.save(noEJBSaved);
+        authorRepository.save(authorOneSaved);
+        authorRepository.save(authorTwoSaved);
+        bookRepository.save(bookOneSaved);
+        bookRepository.save(bookTwoSaved);
 
         System.out.println("In Bootstrap");
         System.out.println("Author Count: " + authorRepository.count());
